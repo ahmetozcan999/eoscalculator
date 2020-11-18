a30731b35b25b106c312d277b4d70c39aaaf6a8f
Ahmet Özcan <f111216079@ktun.edu.tr>
2020-11-18 23:06:17 +0300

    count

--- lib/pages/eospage.dart
+++ lib/pages/eospage.dart
@@ -19,48 +19,96 @@ class EOSPageState extends State<EOSPage> {
   // Note: This is a GlobalKey<FormState>,
   // not a GlobalKey<EOSPageState>.
   final _formKey = GlobalKey<FormState>();
+  double multiplyResult;
 
   @override
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
 
     final provider = Provider.of<AppStateProvider>(context);
+    final multiplyFieldController = TextEditingController();
+
+    count() {
+      setState(() {
+        multiplyResult = double.parse(multiplyFieldController.text) *
+            provider.currentCompound.pc;
+      });
+    }
 
     return Scaffold(
       appBar: AppBar(
         title: Text('EOS Hesaplama'),
       ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text("Seçilen adı : "),
-            Text(provider.currentCompound?.name ?? "-"),
-            TextFormField(
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Validate returns true if the form is valid, or false
-                  // otherwise.
-                  if (_formKey.currentState.validate()) {
-                    // If the form is valid, display a Snackbar.
-                    Scaffold.of(context).showSnackBar(
-                        SnackBar(content: Text('Processing Data')));
-                  }
-                },
-                child: Text('Submit'),
+      body: Builder(
+        builder: (context) => Container(
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                        "Seçilen adı: " + provider.currentCompound?.name ??
+                            "-"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("PC değeri: " +
+                        provider.currentCompound?.pc.toString()),
+                  ),
+                  // TextFormField(
+                  //   validator: (value) {
+                  //     if (value.isEmpty) {
+                  //       return 'Please enter some text';
+                  //     }
+                  //     return null;
+                  //   },
+                  // ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Kaçla çarpılacak?"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: multiplyFieldController,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Validate returns true if the form is valid, or false
+                        // otherwise.
+                        if (_formKey.currentState.validate()) {
+                          // If the form is valid, display a Snackbar.
+
+                          count();
+
+                          Scaffold.of(context).showSnackBar(
+                              SnackBar(content: Text('Hesaplanıyor...')));
+                        }
+                      },
+                      child: Text('Hesapla'),
+                    ),
+                  ),
+                  multiplyResult != null
+                      ? Text("Sonuç : " + multiplyResult.toString())
+                      : Text(""),
+                ],
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
