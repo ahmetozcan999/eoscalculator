86ff6efb5d3a334bc3471af01b317ff9fe1f04ab
Ahmet Özcan <ahmetozcan@Ahmet-MacBook-Pro.local>
2021-02-02 23:00:06 +0300

    onesignal Added,update double values

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Ahmet Özcan on 2.02.2021.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
